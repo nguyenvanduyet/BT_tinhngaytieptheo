82242596356c3f05e39ac721e6856fe7ae282ab2
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2020-12-21 21:07:41 +0700

    update

--- Test/NextDayCalculatorTest.java
+++ Test/NextDayCalculatorTest.java
@@ -14,5 +14,17 @@ public class NextDayCalculatorTest {
         String result = NextDay.nextDayCalculator(day, month, year);
         assertEquals(result, expected);
     }
+    @Test
+    @DisplayName("case 1/1/2018")
+    public void testDay31Month1year2018() {
+        int day = 31;
+        int month = 1;
+        int year = 2018;
+        String expected = "1-2-2018";
+        String result = NextDay.nextDayCalculator(day, month, year);
+        assertEquals(result, expected);
+
+    }
+
 
 }
--- src/NextDay.java
+++ src/NextDay.java
@@ -1,7 +1,34 @@
 public class NextDay {
+    private static final int FIRSTODAY = 1;
+    private static int LASTODAY = 31;
+    private static final String JOIN_CHARACTER = "-";
+
     public static String nextDayCalculator(int day, int month, int year){
-        String result= ++day +"-"+month+"-"+year;
+        if (day==LASTODAY){
+            day=FIRSTODAY;
+            month++;
+        }
+        String result= day +JOIN_CHARACTER+month+JOIN_CHARACTER+year;
         return result;
+    }
+    private static void checkMonth(int month) {
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                LASTODAY = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                LASTODAY = 30;
+                break;
+        }
     }
 
 }
